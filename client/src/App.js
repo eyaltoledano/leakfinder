38a750c45f1995bd9606306dd936cbf1b02a0be3
Eyal Toledano <eutait@gmail.com>
2019-12-21 12:16:52 -0500

    Prepares the call to action to display the App on click.

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,15 @@ import Navigation from './components/navigation'
 function App() {
   return (
     <div className='container mx-auto mt-4'>
-      <Navigation />
-      <p>We're in. Here we go!</p>
+      <div class="flex flex-wrap -mx-4">
+            <div class="w-full mb-6 lg:mb-1 lg:w-full px-4 flex flex-col">
+              <div class="flex-grow flex flex-col bg-white border-t border-b sm:rounded sm:border shadow overflow-hidden">
+                <div class="border-b p-6">
+                Calculator will go here
+                </div>
+              </div>
+            </div>
+      </div>
     </div>
   );
 }
--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import Navigation from '../components/navigation'
+import App from '../App'
 
 class HomePage extends React.Component {
+
+  state = {
+    showApp: false
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      showApp: true
+    })
+  }
+
+  renderCta = () => {
+    // This will need some love with Tailwind. Namely a hero unit
+    return <button ref='cta' onClick={event => this.handleClick(event)}>New Calculation</button>
+  }
+
   render() {
+
     return (
       <div className="container mx-auto p-4 mt-4">
         <p>Welcome to Leakfinder</p>
-        <button>New Calculation</button>
+        {this.state.showApp ? <App /> : this.renderCta()}
       </div>
     )
   }
