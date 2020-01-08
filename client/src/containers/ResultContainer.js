27f675ba8901c7e500dc76a5e0982a4d92f76de0
Eyal Toledano <eutait@gmail.com>
2020-01-08 14:06:32 -0500

    Touches up the fetch URL to match production URL on Heroku.

--- client/src/containers/ResultContainer.js
+++ client/src/containers/ResultContainer.js
@@ -33,7 +33,7 @@ class ResultContainer extends Component {
         body: readyData
       }
 
-      const response = await fetch('http://localhost:3001/api/calculations', requestOptions)
+      const response = await fetch('http://leakfinder.herokuapp.com/api/calculations', requestOptions)
 
       const json = await response.json()
       const id = json.id
@@ -49,7 +49,7 @@ class ResultContainer extends Component {
           requesting: true
         })
 
-        const response = await fetch(`http://localhost:3001/api/calculations/${this.state.calculation.id}/result`)
+        const response = await fetch(`http://leakfinder.herokuapp.com/api/calculations/${this.state.calculation.id}/result`)
 
         const returnedResult = await response.clone().json()
 
